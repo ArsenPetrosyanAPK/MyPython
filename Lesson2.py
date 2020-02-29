e2997447aa7e5dcc3a8e3c63edd8990b1e4a8829
ArsenPetrosianAPK <arsen.apk@mail.ru>
2020-02-29 20:10:40 +0400

    Lesson2

---
+++ Lesson2.py
@@ -0,0 +1,54 @@
+#age = input ('When you was born?')
+#x = 2020-int(age) 
+#print('you are', x, 'years old')
+#radius = int(input("please tell me radius?"))
+#raund = 3.14 + radius + radius
+#print('raund of circle is', raund)
+#word = ("Hello World")
+#print(type(word))
+
+#num = 4234
+#print(type(num))
+
+#num2 = 9.23
+#print(type(num2))
+# type Tesaky (str, int, float)
+#a = 10
+#b = 20 
+#z = a + b
+
+#print(z
+
+#s = 35
+#f = 10 
+#x = s / f 
+#print(x)
+#num = int(input("please enter number"))
+#x = int (input("please enter second number"))
+#print(num - x)
+#a = 4 
+#b = 5
+#print(str(a) + str(b))
+
+#x = 50 
+#y = 25 
+#print(x%y)
+
+
+#x = 112 
+#y = 25 
+#print(x//y)
+
+
+#x = 10 
+#y = 2 
+#print(x**y)
+
+#radius = int(input("please tell me radius?"))
+#raund = 3.14 * radius ** 2
+#print('raund of circle is'+'  '+'' str(raund))
+
+#name = input("what is your name?")
+#old = int(input("how old are you?"))
+#date_was_born = 2020 - old 
+#print("my name is" + ' ' + name + ' ' + "i am "+ str(old) + " years old")
