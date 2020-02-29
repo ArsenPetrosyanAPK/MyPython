d8992e7793f43b15e2610cb082634a60579b28de
ArsenPetrosianAPK <arsen.apk@mail.ru>
2020-02-29 20:11:45 +0400

    lesson3

---
+++ Lesson3.py
@@ -0,0 +1,94 @@
+#num = int(input('first num'))
+#num2 = int(input('second num'))
+#c = num ** 2 + num2 ** 2
+#x = c ** 0.5
+#print(x)
+
+#number = int(input('first number'))
+#number_2 = int(input('second number'))
+#x = number ** 0.5 
+#y = number_2 ** 0.5
+
+
+#h = int(input('enter number'))
+
+#f =int(input("enter number"))
+
+#j = h ** 0.5
+
+#o = f ** 0.5 
+#print(j,o)
+
+#n1 = int(input('number'))
+#n2 = int(input ('number'))
+#r = n1 ** 0.5 
+#d = n2 ** 0.5
+
+#print(r,d)
+#radius = int(input('tell me a radius'))
+
+#a = 2 *3.14* radius ** 2
+#print(a)
+
+#num = int(input("number"))
+#z = num ** 0.5
+#print (z)
+
+#a = int(input('number'))
+#result = a ** 0.5
+#print(result)
+
+#balance = 100
+#paycheck = 150
+#balance = balance + paycheck
+
+#balance = 100 
+#balance = balance + 150 
+#balance += 150
+#print(balance)
+
+#balance = 100 
+#balance = balance + 150 
+#balance -= 150
+#print(balance)
+
+#from math import pi
+
+#radius = int(input('first num'))
+
+#s = 2 * pi * radius**2
+#print(pi)
+
+#import random 
+#x = random.randint(1,6)
+#y = random.randint(1,6)
+#print(x)
+#print(y)
+#print(x + y)
+
+#import random as r
+#print(r.randrange(0, 50, 10))
+
+#import time	
+#time.sleep(10)
+#print('hello world')
+
+#import os 
+#print (os.getcwd())
+
+#import calendar
+#y = int(input('input the year:'))
+#m = int(input('input the mount:'))
+
+#print(calendar.month(y,m))
+#import datetime
+#x = datetime.datetime.now()
+#print(x)
+#print(datetime.date.today())
+
+
+#import datetime 
+#born = int(input('when you was born?'))
+#x = datetime.datetime.now()
+#a = x.year-born
+#print('You are ' + str(a) + ' years old')
