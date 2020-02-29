96d1310d6fe6e5ed56346b2a5411a0d2077bf3e8
ArsenPetrosianAPK <arsen.apk@mail.ru>
2020-02-29 20:13:48 +0400

    lesson5

---
+++ Lesson5.py
@@ -0,0 +1,51 @@
+#print('please input (y) for right, othervise (n)')
+#item_kiwi = input("have you kiwi in refigerator?") == "y"
+
+#item_apple = input('have you apple in refigerator') == 'y'
+
+#item_orange = input('have you orange in refigerator') == 'y'
+
+#item_tomatto = input ('have you tomatto in the refigerator') == 'y'
+
+#item_pear = input ('have you pear in the refigerator') == 'y'
+#print(item_kiwi)
+#print(item_apple)
+#print(item_orange)
+#print(item_tomatto)
+#print(item_pear)
+
+#home_electricity = input (' have you electricity in home') == 'y'
+
+
+
+#remove_kiwi = item_kiwi and not home_electricity
+#remove_apple = item_apple and not home_electricity
+#remove_tomatto = item_tomatto and not home_electricity
+#remove_pear = item_pear and not home_electricity
+#remove_orange = item_orange and not home_electricity
+#print('remove kiwi ' +  str(remove_kiwi))
+#print('remove apple ' +  str(remove_apple))
+#print('remove tomatto ' +  str(remove_tomatto))
+#print('remove pear ' +  str(remove_pear))
+#print('remove orang ' +  str(remove_orange))
+
+#print('go bring kiwi' + str(not item_kiwi))
+#print('go bring apple' + str(not item_apple))
+#print('go bring tomatto' + str(not item_tomatto))
+#print('go bring orange' + str(not item_orange))
+#print('go bring pear' + str(not item_pear))
+
+
+
+#print('You have a iPhone if yes plese input (y) if no plese input (n)')
+#iphone = input('you have a iPhone') == 'y'
+#print('you have  iPhones ' + str(iphone))
+#iphone_color = input('what color is your iphone ') == 'red'
+#iphone_red = iphone and  iphone_color
+#print('your iPhone is red' + str(iphone_red))
+
+#iphone_model = input('what is your iPhone model ') == '7'
+#iphone7 = iphone and iphone_model 
+#print('your iphone is 7' + str(iphone7))
+
+
