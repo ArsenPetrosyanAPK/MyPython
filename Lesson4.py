d27e661d9b5679ab24a1d07072462e82cc2ce48a
ArsenPetrosianAPK <arsen.apk@mail.ru>
2020-02-29 20:13:11 +0400

    lesson4

---
+++ Lesson4.py
@@ -0,0 +1,111 @@
+#import random
+#x = (random.randint(1,6))
+#y = (random.randint(1,6))
+#print(x,y)
+
+#import time
+#time.sleep(2)
+#print('hello world')
+
+
+#import random as r 
+#print(r.randrange(0, 50, 10))
+
+
+#from math import pi 
+#radius = int(input('first num'))
+#s = 2 * pi * radius ** 2
+#print(pi)
+
+
+#import calendar
+#y = int(input('input the year:'))
+#m = int(input('input the mount:'))
+#print(calendar.month(y,m))
+
+#import random as r
+#x = (r.randint(1,5))
+#print(x)
+
+#import random as r
+#a = int(input("enter any number"))
+#b = r.randint (1,9)
+#print(a+b)
+
+#Lesson 4 
+
+#import time
+#a = int(input('enter any number'))
+#time.sleep(2)
+#print(5 > a)
+
+#x = int(input('enter any number'))
+#time.sleep(2)
+#print(5 == x)
+
+#y = int(input('enter any number'))
+#time.sleep(2)
+#print(20 < y)
+
+#g = int(input('number'))
+#print(h != 5)
+
+#import random 
+#name = str(input('What is your name?'))
+#print('wellcome to the mini game ', name)
+#a1 = int(input('Enter any number'))
+#a2 = int(input('enter any number'))
+#b1 = random.randint (1, 100)
+#b2 = random.randint (1, 100)
+#x = a1 + a2
+#y = b1 + b2
+#print(x > y)
+
+#name = (input('What is your name?'))
+#num = int(input('how old are you?'))
+#print(num > 18 and num < 27)
+
+#name = (input('What is your name?'))
+#num = int(input('how old are you?'))
+#print(num < 18 or num > 27)
+
+#num = int(input('how old are you?'))
+#print(not(num > 18 and num < 27))
+
+
+
+
+#name = "Ani"
+#name2 = "Anna"
+#print(name is name2)
+
+
+
+
+#name = (input('What is your name?'))
+#name2 = 'Anna'
+#print(id(name2))
+#print(id(name))
+#print(name == 'anna' or name =='Anna' or 'ANNA')
+
+
+
+
+
+#x = (input('what is the longest river in the world?'))
+#print(x == 'nexos' or x == 'Nexos' or x == 'NEXOS' )
+
+#x = (input('what is the longest river in the world?'))
+#print(x == 'nexos' and x == 'Nexos' and x == 'NEXOS' )
+
+
+#name = 'Ani'
+#name2 = 'Anna'
+#print('J' not in name2)
+
+
+#name = input('what is your name?')
+#print('a' in name)
+
+#dasaran = 'Arsen Levon Vardan Jora Suren'
+#print('Arsen' not in dasaran and 'Levon' in 'Vardan' in dasaran) 
